13dfc6b8ea8013f92847c795bae85eb4696f69a7
David Madl <git@abanbytes.eu>
2016-02-25 12:24:55 +0100

    Mmsapt multi-index: load domain specific indexes
    Optionally load up domain specific indexes built with mtt-build -m.

--- moses/TranslationModel/UG/mm/ug_mm_bitext.h
+++ moses/TranslationModel/UG/mm/ug_mm_bitext.h
@@ -9,7 +9,12 @@ namespace sapt
     void load_document_map(std::string const& fname);
   public:
     void open(std::string const base, std::string const L1, std::string L2);
+    void open_domain_indexes(std::string const base, std::string const L1, std::string L2);
     mmBitext();
+
+  private:
+    std::vector<SPTR<TSA<TKN> > > m_domainI1; // I1 indexes, separately for each domain
+    std::vector<SPTR<TSA<TKN> > > m_domainI2; // I2 indexes, separately for each domain
   };
 
   template<typename TKN>
@@ -72,10 +77,44 @@ namespace sapt
     i2.open(base+L2+".sfa", this->T2);
     assert(this->T1->size() == this->T2->size());
 
-    std::string docmapfile = base+"dmp";
-    if (!access(docmapfile.c_str(),F_OK))
+    std::string docmapfile = base + "dmp";
+    if (!access(docmapfile.c_str(), F_OK))
       load_document_map(docmapfile);
+
+    open_domain_indexes(base, L1, L2);
   }
 
+  template<typename TKN>
+  void
+  mmBitext<TKN>::open_domain_indexes(std::string const base, std::string const L1, std::string L2)
+  {
+    // no docmap provided, no domain-specific indexes
+    if(this->m_docname.size() == 0)
+      return;
+
+    // if the first document's index does not exist, we assume there are no domain-specific indexes
+    std::string firstidx = base + L1 + "." + this->m_docname[0] + ".sfa";
+    if(access(firstidx.c_str(), F_OK))
+      return;
+
+    // for each domain, load the domain-specific TSA index (for both corpus sides).
+    std::vector<std::string>::iterator d;
+    for(d = this->m_docname.begin(); d != this->m_docname.end(); d++)
+    {
+      std::string idx1 = base + L1 + "." + *d + ".sfa";
+      std::string idx2 = base + L2 + "." + *d + ".sfa";
+      assert(!access(idx1.c_str(), F_OK));
+      assert(!access(idx2.c_str(), F_OK));
+
+      SPTR<TSA<TKN> > di1(new mmTSA<TKN>());
+      SPTR<TSA<TKN> > di2(new mmTSA<TKN>());
+      mmTSA<TKN>& i1 = *reinterpret_cast<mmTSA<TKN>*>(di1.get());
+      mmTSA<TKN>& i2 = *reinterpret_cast<mmTSA<TKN>*>(di2.get());
+      i1.open(idx1, this->T1);
+      i2.open(idx2, this->T2);
+      m_domainI1.push_back(di1);
+      m_domainI2.push_back(di2);
+    }
+  }
 }
 
