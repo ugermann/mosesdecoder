ugermann/mosesdecoder

commit be7661b346d0c79b8c0dc879e182c1678af9fcb4

Mmsapt multi-index preparation: remove bitext from expand()
* avoid using tree_iterator in expand()
* remember target phrase in jstats

moses/TranslationModel/UG/mm/ug_bitext_agenda_worker.h
@@ -78,6 +78,8 @@ Bitext<Token>::agenda
 	      SPTR<iter> b = I.find(o + s, e1 - s);
 	      UTIL_THROW_IF2(!b || b->size() < e1-s, "target phrase not found");
 
+              phrase<Token> trg(o + s, e1 - s);
+
 	      for (size_t i = e1; i <= e2; ++i)
 		{
 		  uint64_t tpid = b->getPid();
@@ -90,7 +92,7 @@ Bitext<Token>::agenda
 
 		  size_t raw2 = b->approxOccurrenceCount();
 		  float bwgt = j->m_bias ? (*j->m_bias)[sid] : 1;
-		  j->stats->add(tpid, sample_weight, bwgt, aln, raw2,
+		  j->stats->add(tpid, (phrase<id_type> &) trg, sample_weight, bwgt, aln, raw2,
 				po_fwd, po_bwd, docid);
 		  bool ok = (i == e2) || b->extend(o[i].id());
 		  UTIL_THROW_IF2(!ok, "Could not extend target phrase.");

moses/TranslationModel/UG/mm/ug_bitext_sampler.h
@@ -584,7 +584,9 @@ consider_sample(TokenPosition const& p)
       TSA<Token> const& I = m_fwd ? *m_bitext->I2 : *m_bitext->I1;
       SPTR<tsa_iter> b = I.find(o + s, rec.e1 - s);
       UTIL_THROW_IF2(!b || b->size() < rec.e1 - s, "target phrase not found");
-	
+
+      phrase<Token> trg(o + s, rec.e1 - s);
+
       for (size_t i = rec.e1; i <= rec.e2; ++i)
         {
           uint64_t tpid = b->getPid();
@@ -592,7 +594,7 @@ consider_sample(TokenPosition const& p)
             continue; // don't over-count
           seen.push_back(tpid);
           size_t raw2 = b->approxOccurrenceCount();
-          size_t evid = m_stats->add(tpid, sample_weight, 
+          size_t evid = m_stats->add(tpid, (phrase<id_type> &) trg, sample_weight,
                                      m_bias ? (*m_bias)[p.sid] : 1, 
                                      aln, raw2, rec.po_fwd, rec.po_bwd, docid);
           max_evidence = std::max(max_evidence, evid);

moses/TranslationModel/UG/mm/ug_bitext.h
@@ -755,23 +755,19 @@ namespace sapt
 
   template<typename Token>
   void
-  expand(typename Bitext<Token>::iter const& m,
+  expand(pid_type pid,
+         phrase<Token> src,
+         bool fwd,
          Bitext<Token> const& bt, pstats const& ps,
          std::vector<PhrasePair<Token> >& dest, std::ostream* log)
   {
-    bool fwd = m.root == bt.I1.get();
     dest.reserve(ps.trg.size());
     PhrasePair<Token> pp;
-    pp.init(m.getPid(), !fwd, m.getToken(0), m.size(), &ps, 0);
-    // cout << HERE << " "
-    // << toString(*(fwd ? bt.V1 : bt.V2), pp.start1,pp.len1) << std::endl;
+    pp.init(pid, !fwd, src.begin, src.len, &ps, 0);
     pstats::trg_map_t::const_iterator a;
     for (a = ps.trg.begin(); a != ps.trg.end(); ++a)
       {
-        uint32_t sid,off,len;
-        parse_pid(a->first, sid, off, len);
-        pp.update(a->first, (fwd ? bt.T2 : bt.T1)->sntStart(sid)+off,
-                  len, a->second);
+        pp.update(a->first, a->second);
         dest.push_back(pp);
       }
   }

moses/TranslationModel/UG/mm/ug_bitext_jstats.cc
@@ -8,6 +8,8 @@ namespace sapt
   float    jstats::bcnt() const { return my_bcnt; }
   uint32_t jstats::cnt2() const { return my_cnt2; }
 
+  phrase<id_type> const& jstats::trg() const { return my_trg; }
+
   // What was that used for again? UG
   bool jstats::valid() { return my_wcnt >= 0; }
   void jstats::validate()   { if (my_wcnt < 0) my_wcnt *= -1; }
@@ -70,7 +72,8 @@ namespace sapt
 
   size_t
   jstats::
-  add(float w, float b, std::vector<unsigned char> const& a, uint32_t const cnt2,
+  add(phrase<id_type> const& trg, // target phrase
+      float w, float b, std::vector<unsigned char> const& a, uint32_t const cnt2,
       uint32_t fwd_orient, uint32_t bwd_orient, int const docid)
   {
     boost::lock_guard<boost::mutex> lk(this->lock);
@@ -78,6 +81,7 @@ namespace sapt
     my_rcnt += 1;
     my_wcnt += w;
     my_bcnt += b;
+    my_trg = trg;
     if (a.size())
       {
         size_t i = 0;

moses/TranslationModel/UG/mm/ug_bitext_jstats.h
@@ -3,7 +3,9 @@
 #include <string>
 #include <stdint.h>
 #include "ug_typedefs.h"
-#include "ug_lexical_reordering.h"
+#include "ug_lexical_reordering.h"m
+// for struct phrase
+#include "ug_ttrack_base.h"
 #include <boost/thread.hpp>
 
 namespace sapt
@@ -17,7 +19,8 @@ namespace sapt
     uint32_t my_rcnt; // unweighted joint count
     uint32_t my_cnt2; // raw counts L2
     float    my_wcnt; // weighted joint count
-    float    my_bcnt; // cumulative bias 
+    float    my_bcnt; // cumulative bias
+    phrase<id_type> my_trg; // target phrase
 
     // to do: use a static alignment pattern store that stores each pattern only
     // once, so that we don't have to store so many alignment std::vectors
@@ -36,11 +39,13 @@ namespace sapt
     uint32_t cnt2() const; // raw target phrase occurrence count
     float    wcnt() const; // weighted joint counts
     float    bcnt() const; // cumulative bias scores
+    phrase<id_type> const& trg() const; // target phrase
 
     std::vector<std::pair<size_t, std::vector<unsigned char> > > const & aln() const;
 
     size_t 
-    add(float w, float b, std::vector<unsigned char> const& a, uint32_t const cnt2, 
+    add(phrase<id_type> const& trg, // target phrase
+        float w, float b, std::vector<unsigned char> const& a, uint32_t const cnt2,
         uint32_t fwd_orient, uint32_t bwd_orient, int const docid);
 
     void invalidate();

moses/TranslationModel/UG/mm/ug_bitext_pstats.cc
@@ -66,7 +66,7 @@ namespace sapt
 
   size_t
   pstats::
-  add(uint64_t pid, float const w, float const b,
+  add(pid_type pid, phrase<id_type> const& trg, float const w, float const b,
       std::vector<unsigned char> const& a,
       uint32_t const cnt2,
       uint32_t fwd_o,
@@ -74,12 +74,12 @@ namespace sapt
   {
     boost::lock_guard<boost::mutex> guard(this->lock);
     jstats& entry = this->trg[pid];
-    size_t ret = entry.add(w, b, a, cnt2, fwd_o, bwd_o, docid);
+    size_t ret = entry.add(trg, w, b, a, cnt2, fwd_o, bwd_o, docid);
     if (this->good < entry.rcnt())
-      {
-        UTIL_THROW(util::Exception, "more joint counts than good counts:"
-                   << entry.rcnt() << "/" << this->good << "!");
-      }
+    {
+      UTIL_THROW(util::Exception, "more joint counts than good counts:"
+                                  << entry.rcnt() << "/" << this->good << "!");
+    }
     return ret;
   }
 

moses/TranslationModel/UG/mm/ug_bitext_pstats.h
@@ -42,7 +42,8 @@ namespace sapt
     size_t count_workers() { return in_progress; }
 
     size_t
-    add(uint64_t const  pid, // target phrase id
+    add(pid_type const  pid, // target phrase id
+        phrase<id_type> const& trg, // target phrase
         float const       w, // sample weight (1./(# of phrases extractable))
         float const       b, // sample bias score
         alnvec const&     a, // local alignment

moses/TranslationModel/UG/mm/ug_phrasepair.h
@@ -49,8 +49,7 @@ namespace sapt
 	      pstats const* ps = NULL, size_t const numfeats=0);
 
     PhrasePair const&
-    update(uint64_t const pid2, Token const* x,
-	   uint32_t const len, jstats const& js);
+    update(uint64_t const pid2, jstats const& js);
 
     void
     fill_lr_vec(LRModel::Direction const& dir,
@@ -105,11 +104,10 @@ namespace sapt
   template<typename Token>
   PhrasePair<Token> const&
   PhrasePair<Token>
-  ::update(uint64_t const pid2,
-	   Token const* x, uint32_t const len, jstats const& js)
+  ::update(uint64_t const pid2, jstats const& js)
   {
     p2    = pid2;
-    start2 = x; len2 = len;
+    start2 = reinterpret_cast<const phrase<Token> &>(js.trg()).begin; len2 = (uint32_t) js.trg().len;
     raw2  = js.cnt2();
     joint = js.rcnt();
     cum_bias = js.bcnt();

commit 3e8fed8773a7b1cc296a72db248f77536ed623ee

Mmsapt: bugfix: fix multiple identical target phrases being reported
Do not try to be smart in obscure code with one-letter variables you haven't written.

Fixes the issue introduced in be7661b346d0c79b8c0dc879e182c1678af9fcb4
in BitextSampler::consider_sample() and Bitext::agenda::worker::operator().

moses/TranslationModel/UG/mm/ug_bitext_agenda_worker.h
@@ -78,8 +78,6 @@ Bitext<Token>::agenda
 	      SPTR<iter> b = I.find(o + s, e1 - s);
 	      UTIL_THROW_IF2(!b || b->size() < e1-s, "target phrase not found");
 
-              phrase<Token> trg(o + s, e1 - s);
-
 	      for (size_t i = e1; i <= e2; ++i)
 		{
 		  uint64_t tpid = b->getPid();
@@ -91,6 +89,7 @@ Bitext<Token>::agenda
 		  seen.push_back(tpid);
 
 		  size_t raw2 = b->approxOccurrenceCount();
+                  phrase<Token> trg(o + s, i - s);
 		  float bwgt = j->m_bias ? (*j->m_bias)[sid] : 1;
 		  j->stats->add(tpid, (phrase<id_type> &) trg, sample_weight, bwgt, aln, raw2,
 				po_fwd, po_bwd, docid);

moses/TranslationModel/UG/mm/ug_bitext_sampler.h
@@ -794,8 +794,6 @@ consider_sample(TokenPosition const& p, SPTR<TSA<Token> > i1, SPTR<TSA<Token> > 
       SPTR<tsa_iter> b = I.find(o + s, rec.e1 - s);
       UTIL_THROW_IF2(!b || b->size() < rec.e1 - s, "target phrase not found");
 
-      phrase<Token> trg(o + s, rec.e1 - s);
-
       for (size_t i = rec.e1; i <= rec.e2; ++i)
         {
           uint64_t tpid = b->getPid();
@@ -803,6 +801,7 @@ consider_sample(TokenPosition const& p, SPTR<TSA<Token> > i1, SPTR<TSA<Token> > 
             continue; // don't over-count
           seen.push_back(tpid);
           size_t raw2 = b->approxOccurrenceCount();
+          phrase<Token> trg(o + s, i - s);
           size_t evid = m_stats->add(tpid, (phrase<id_type> &) trg, sample_weight,
                                      m_bias ? (*m_bias)[p.sid] : 1, 
                                      aln, raw2, rec.po_fwd, rec.po_bwd, docid);